3a1f21a82de89244b02ae4e9d5d16b286977c387
Andrey Testov <avtestov@ya.ru>
2021-11-05 14:42:43 +0300

    Добавил чтение файла с данными

--- data-import.php
+++ data-import.php
@@ -2,5 +2,20 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Обработка импорта данных");
 
+$file = fopen($_FILES['import']["tmp_name"], 'r') or die("не удалось открыть файл");
+#Получение списка полей
+$Fields = explode(",", trim(fgets($file)));
+
+#Получение данных
+$Users = array();
+while(!feof($file))
+{
+	#Создаем ассоциативный массив. 
+	#	Компания	=> НПП Промавтоматика
+	#	Email		=> suleymanov-promav@nowmedia.ru
+	#	Поле		=> Значение
+	$Users[] =  array_combine($Fields, explode(",", trim(fgets($file))));
+}
+fclose($file);
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
