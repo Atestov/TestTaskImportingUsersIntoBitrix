de024ec251ccbf7d17004b5cc77d551f9dd1038d
Andrey Testov <avtestov@ya.ru>
2021-11-05 12:20:39 +0300

    Добавил загрузку файла для импорта данных пользователей

--- import-polzovateley.php
+++ import-polzovateley.php
@@ -1,4 +1,12 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Импорт пользователей");
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+
+<form enctype="multipart/form-data" action="/data-import.php" method="POST">
+    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+    Отправить файл: <input name="import" type="file" />
+    <input type="submit" value="Отправить файл" />
+</form>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
